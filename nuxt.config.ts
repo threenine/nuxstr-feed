dffd31ee4c951e592bf13355623b6dd4b8bea745
Gary Woodfine <lnb0l9dc@duck.com>
2025-09-18 15:00:27 +0100

    **Initialize project with configuration files and dependencies** - Added `.editorconfig` for consistent coding styles - Created `package.json` with initial project metadata and dependencies - Included `LICENSE` file with MIT license

---
+++ nuxt.config.ts
@@ -0,0 +1,10 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  modules: [
+    '@nuxt/content',
+    '@nuxt/eslint',
+    '@nuxt/ui',
+    '@nuxt/test-utils',
+    '@nuxt/scripts',
+  ],
+})
